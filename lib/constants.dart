1c19fd3fd7586e068ea8c05bbd3a66f5e5752ff5
Administrator <hemo089@outlook.com>
2023-12-27 21:48:50 +0200

    finish home page

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:ezy_buy/core/utils/app_images.dart';
+import 'package:ezy_buy/features/home_page/data/models/cateogy_model.dart';
 import 'package:flutter/material.dart';
 
 class AppColor {
@@ -14,5 +15,21 @@ class AppConstants {
   static List banners = [
     AppImages.banner1,
     AppImages.banner2,
+  ];
+  static List<CategoryModel> categoriesNamed = [
+    CategoryModel(
+        id: AppImages.cosmetics, image: AppImages.cosmetics, name: "cosmetics"),
+    CategoryModel(id: AppImages.book, image: AppImages.book, name: "book"),
+    CategoryModel(
+        id: AppImages.electronics,
+        image: AppImages.electronics,
+        name: "electronics"),
+    CategoryModel(
+        id: AppImages.fashion, image: AppImages.fashion, name: "fashion"),
+    CategoryModel(
+        id: AppImages.mobiles, image: AppImages.mobiles, name: "mobiles"),
+    CategoryModel(id: AppImages.pc, image: AppImages.pc, name: "pc"),
+    CategoryModel(id: AppImages.shoes, image: AppImages.shoes, name: "shoes"),
+    CategoryModel(id: AppImages.watch, image: AppImages.watch, name: "watch"),
   ];
 }
--- lib/features/home_page/presenation/view/widgets/home_page_body.dart
+++ lib/features/home_page/presenation/view/widgets/home_page_body.dart
@@ -1,3 +1,5 @@
+import 'package:ezy_buy/constants.dart';
+import 'package:ezy_buy/core/utils/app_images.dart';
 import 'package:ezy_buy/features/home_page/presenation/view/widgets/card_swiper.dart';
 import 'package:ezy_buy/features/home_page/presenation/view/widgets/latest_arrival_list.dart';
 import 'package:flutter/material.dart';
@@ -9,15 +11,79 @@ class HomepageBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        children: [
-          CardSwiperItems(),
-          SizedBox(
-            height: 15,
-          ),
-          LatestArrivalList(),
-        ],
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CardSwiperItems(),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Lates Arrival",
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
+            ),
+            LatestArrivalList(),
+            Text(
+              "Categories",
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            GridCategories(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class GridCategories extends StatelessWidget {
+  const GridCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 4,
+      children: List.generate(AppConstants.categoriesNamed.length, (index) {
+        return CategoryItem(
+            image: AppConstants.categoriesNamed[index].image,
+            name: AppConstants.categoriesNamed[index].name);
+      }),
+    );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.image, required this.name});
+  final String image, name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          image,
+          height: 50,
+          width: 50,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          name,
+          maxLines: 1,
+          style: const TextStyle(
+            overflow: TextOverflow.ellipsis,
+            fontSize: 19,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
     );
   }
 }

256ffbbeb3a9694111e0952537e3b2b09b166cc2
Administrator <hemo089@outlook.com>
2023-12-31 00:37:46 +0200

    fix dark mode

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -4,20 +4,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ThemeProvider with ChangeNotifier {
   bool _isDark = false;
   bool get isDarkMode => _isDark;
-  static String themeStatus = "status";
+  static const themeStatus = "status";
   ThemeProvider() {
     getThemeMode();
   }
   Future<void> setThemeMode({required bool themeValue}) async {
     SharedPreferences sharedPref = await SharedPreferences.getInstance();
-    sharedPref.setBool(ThemeProvider.themeStatus, themeValue);
+    sharedPref.setBool(themeStatus, themeValue);
     _isDark = themeValue;
     notifyListeners();
   }
 
   Future<bool> getThemeMode() async {
     SharedPreferences sharedPref = await SharedPreferences.getInstance();
-    sharedPref.get(ThemeProvider.themeStatus);
+    _isDark = sharedPref.getBool(themeStatus) ?? false;
     notifyListeners();
     return _isDark;
   }
